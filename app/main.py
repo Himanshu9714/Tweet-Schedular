9de794f9661400f3e63cc5a21c95ab25a1ccdfe8
Himanshu Suthar <h9714348433@gmail.com>
2022-07-17 15:04:57 +0530

    Add tweet example

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 import gspread
 from dotenv import load_dotenv
 import os
+from datetime import datetime, timedelta
 
 load_dotenv()
 
@@ -36,3 +37,45 @@ def tweet_list():
 
     n_open_tweets = sum(1 for tweet in tweets if not tweet.done)
     return render_template('base.html', tweets=tweets, n_open_tweets=n_open_tweets)
+
+
+def get_date_time(date_time_str):
+    date_time_obj = None
+    error_code = None
+    try:
+        date_time_obj = datetime.strptime(date_time_str, "%Y-%m-%d %H:%M:%S")
+    except ValueError as e:
+        error_code = f"Error! {e}"
+
+    if date_time_obj is not None:
+        now_time_india = datetime.utcnow() + timedelta(hours=5, minutes=30)
+        if not date_time_obj > now_time_india:
+            error_code = f"Error! time must be in the future"
+    return date_time_obj, error_code
+
+
+@app.route("/tweet", methods=['POST'])
+def add_tweet():
+    message = request.form['message']
+    if not message:
+        return "No message"
+    
+    if len(message) > 280:
+        return "Message too long! It should <= 280 characters"
+
+    time = request.form['time']
+    if not time:
+        return "No time entered"
+
+    password = request.form['password']
+    if not password or password != "12345":
+        return "Wrong password!"
+
+    date_time_obj, error_code = get_date_time(time)
+    if error_code is not None:
+        return error_code
+    
+    tweet = [str(date_time_obj), message, 0]
+    worksheet.append_row(tweet)
+
+    return redirect('/')
